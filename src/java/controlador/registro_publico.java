15913ea091d40a42f20fd298fb6c7006c81fc8f3
Manuel Ruiz <ruiz_r_manuel@hotmail.com>
2017-01-05 03:24:45 -0600

    Merge origin/master
    Conflicts:
    	web/registro_admin.jsp

---
+++ src/java/controlador/registro_publico.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author trafalgar
+ */
+@WebServlet(name = "registro_publico", urlPatterns = {"/registro_publico"})
+public class registro_publico extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        
+        
+                  String nombre = request.getParameter("nombre");
+                  String domicilio = request.getParameter("domicilio");
+                  String  telefono_contacto = request.getParameter("telefono_contacto");
+                  String correo = request.getParameter("correo");
+                              
+                  
+                  modelo.Publico registro = new modelo.Publico();
+       
+
+                 registro.setId(0);
+                 registro.setNombre(nombre);
+                 registro.setDomicilio(domicilio);
+                 registro.setTelefono(correo);
+                 registro.setCorreo(correo);
+                    
+          EntityManager em;
+          EntityManagerFactory emf;
+          emf = Persistence.createEntityManagerFactory("proyecto_cocoPU");
+          em =emf.createEntityManager();
+          em.getTransaction().begin();
+          em.persist(registro);
+          em.flush();
+          em.getTransaction().commit();
+          em.close();
+          emf.close();
+          response.sendRedirect("correcto.jsp");
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/registro_publico.jsp
@@ -0,0 +1,41 @@
+<%-- 
+    Document   : registro_publico
+    Created on : Jan 5, 2017, 3:10:05 AM
+    Author     : trafalgar
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        
+        
+       <form action="registro" method="post">
+        Nombre:
+                <input type="text" name="nombre">
+                <br/>
+        Apellido:
+                <input type="text" name="apellido">
+                <br/>
+        Domicilio:
+                  <input type="text" name="domicilio">
+                  <br/>
+        Telefono:
+                <input type="text" name="telefono_contacto">
+                <br/>
+        Nombre Conferencia:
+                 <input type="text" name="nombre_conferencia">
+                <br/>
+        Correo:
+                <input type="text" name="correo">
+                <br/>
+    <p><input type="submit" value="Enviar"></p>
+</form>
+        
+        
+    </body>
+</html>
