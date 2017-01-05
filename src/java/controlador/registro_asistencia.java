077b9711df1bbc777dd0fcec247cf4c32fdf21eb
Manuel Ruiz <ruiz_r_manuel@hotmail.com>
2017-01-05 05:00:08 -0600

    registro asistencia

---
+++ src/java/controlador/registro_asistencia.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Asistencia;
+import modelo.Conferencia;
+import modelo.Usuario;
+
+/**
+ *
+ * @author trafalgar
+ */
+@WebServlet(name = "registro_asistencia", urlPatterns = {"/registro_asistencia"})
+public class registro_asistencia extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+                       
+            
+            if(request.getParameter("id") != null) {
+                int id = Integer.parseInt(request.getParameter("id"));
+                int codigo = Integer.parseInt(request.getParameter("codigo"));
+                
+                EntityManagerFactory emf = Persistence.createEntityManagerFactory( "Proyecto_CocoPU" );
+                EntityManager em = emf.createEntityManager();
+                modelo.Publico publico  = em.find(modelo.Publico.class, id);
+                if(publico != null) {
+                    Asistencia asistencia = new Asistencia();
+                    asistencia.setId(0);
+                    asistencia.setIdConferencia(id);
+                    asistencia.setIdUsuario(codigo);
+                    
+                    em.getTransaction().begin();
+                    em.persist(asistencia);
+                    em.getTransaction().commit();
+                    em.close();
+                    emf.close();
+                    response.sendRedirect("correcto.jsp");
+                }
+                else {
+                    response.sendRedirect("registro_asistencia.jsp");
+                }
+                  
+            }    
+                
+            
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- web/registro_asistencia.jsp
+++ web/registro_asistencia.jsp
@@ -8,15 +8,45 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <%@ page import="java.util.List,
+                javax.persistence.EntityManagerFactory,
+                javax.persistence.EntityManager,
+                modelo.Conferencia,
+                javax.persistence.Persistence,
+                javax.persistence.Query" %>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>JSP Page</title>
     </head>
     <body>
         
+        <form action="registro_asistencia" method="get">
+            Conferencia:
+                <select name="id">
+                    <%
+                        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Proyecto_CocoPU" );
+                        EntityManager entitymanager = emfactory.createEntityManager();
+
+                        Query query = entitymanager.createQuery("Select c From Conferencia c ");
+                        List<Conferencia> conferencias = query.getResultList();
+
+                        for(Conferencia conferencia : conferencias) {
+                    %> 
+                    <option value=<%= conferencia.getId() %>><%= conferencia.getNombre() %></option>
+                    <%
+                        }
+                    %>
+                </select>
+                Codigo:
+                <input type="text" name="codigo">
+                <br/>
+                <div>
+                 <input type="submit" name="submit" value="Enviar"/>
+                </div>
+        </form>
         
         
-         <h1>El Usuario se ha registrado con exito</h1>
-        <a href="index.xhtml" >Inicio</a>
+        
+        
         
         
     </body>
